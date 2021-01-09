89538802c804d42838b1a70f829bee49b585a7b7
Midhet Sulemani <midhetfatema94@gmail.com>
2021-01-09 18:34:51 +0300

    Initial commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Waveline Media on 1/9/21.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
